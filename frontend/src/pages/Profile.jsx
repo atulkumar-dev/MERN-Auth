34bbce42166335be50913b59c382bb051b4b1cd1
Atul Kumar <atulkumar.works@gmail.com>
2025-07-04 10:14:57 +0530

    add onClick handler for Signout button in Profile component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,11 +5,16 @@ export default function Profile() {
   const username = localStorage.getItem("username");
   const email = localStorage.getItem("email");
 
+  function handleSignup(){
+    
+
+  }
+
   return <>
   <div className="flex flex-col text-4xl items-center justify-center min-h-screen">
   <div>User Email: {email}</div>
   <div>Username: {username}</div>
-  <button className=" border-2 rounded-2xl p-4 hover:cursor-pointer mt-4 bg-blue-600 text-white">Signout</button>
+  <button onClick={handleSignup} className=" border-2 rounded-2xl p-4 hover:cursor-pointer mt-4 bg-blue-600 text-white">Signout</button>
 </div>
   </>;
 }
