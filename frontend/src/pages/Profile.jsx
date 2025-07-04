fc5bca0a9220eb41bc2d976b223f23e7853227ed
Atul Kumar <atulkumar.works@gmail.com>
2025-07-04 10:15:32 +0530

    fix: rename handleSignup to handleSignout in Profile component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ export default function Profile() {
   const username = localStorage.getItem("username");
   const email = localStorage.getItem("email");
 
-  function handleSignup(){
+  function handleSignout(){
     
 
   }
@@ -14,7 +14,7 @@ export default function Profile() {
   <div className="flex flex-col text-4xl items-center justify-center min-h-screen">
   <div>User Email: {email}</div>
   <div>Username: {username}</div>
-  <button onClick={handleSignup} className=" border-2 rounded-2xl p-4 hover:cursor-pointer mt-4 bg-blue-600 text-white">Signout</button>
+  <button onClick={handleSignout} className=" border-2 rounded-2xl p-4 hover:cursor-pointer mt-4 bg-blue-600 text-white">Signout</button>
 </div>
   </>;
 }
